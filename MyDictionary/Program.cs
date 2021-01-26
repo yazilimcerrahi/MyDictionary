2a893b457cc92ba1e52d33c1a097964c6530cfeb
Yusuf Dağ <59394958+rosykick@users.noreply.github.com>
2021-01-26 18:38:09 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Yusuf");
+            isimler.Add("Erol");
+            isimler.Add("Kaan");
+            isimler.Add("Emin");
+            isimler.Add("Celal");
+            isimler.Add("Mert");
+            Console.WriteLine(isimler.Count);
+
+        }
+    }
+
+    class MyDictionary<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyDictionary()
+        {
+            _array = new T[0];
+        }
+        public void Add(T isim)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length +1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = isim;
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
